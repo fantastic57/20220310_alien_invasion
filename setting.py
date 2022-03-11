56f2cfedc682d50220548a5e2a7f28e5bf292fc6
fantastic57 <tlwuqi5757@gmail.com>
2022-03-11 14:45:11 +0800

    参数小修正

--- setting.py
+++ setting.py
@@ -10,7 +10,7 @@ class Settings:
         # 飞船设置
         self.ship_limit = 3  # 三次机会
         # 子弹设置
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3  # 子弹最多个数
@@ -24,7 +24,7 @@ class Settings:
         self.ship_speed = 1.5
         self.bullet_speed = 1.5
         self.fleet_drop_speed = 3  # 外星人垂直移动速度
-        self.alien_speed = 0.5  # 外星人水平移动速度
+        self.alien_speed = 0.2  # 外星人水平移动速度
         self.fleet_direction = 1  # 1为右移，-1为左移
         self.alien_points = 10  # 每个外星人多少分
 
